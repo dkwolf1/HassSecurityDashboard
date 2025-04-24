dbc2f5515aa58e9c5504065dc96e6e2b81fac1fc
Dennis <102389032+dkwolf1@users.noreply.github.com>
2025-04-24 11:11:54 +0200

    Delete hass_security_dashboard directory

--- hass_security_dashboard/app/scanners/ports.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-def check():
- try:
-  r = subprocess.check_output(['ss', '-tuln'], text=True)
-  return {'status': 'ok', 'log': r}
- except Exception as e:
-  return {'status': 'fail', 'log': str(e)}
