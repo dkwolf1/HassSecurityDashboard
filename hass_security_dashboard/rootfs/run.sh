50cfa3f4d2857d8d726c54989163e47e5a536d51
Dennis <102389032+dkwolf1@users.noreply.github.com>
2025-04-25 20:37:28 +0200

    Update run.sh

--- hass_security_dashboard/rootfs/run.sh
+++ hass_security_dashboard/rootfs/run.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/with-contenv bash
-# Start security-dashboard service
+set -e
+
+# Kies tussen ingress of standalone HTTP
 if [ "${USE_INGRESS}" = "false" ]; then
   export FLASK_RUN_PORT=${PORT:-8123}
   flask --app /app/dashboard run --host 0.0.0.0
 else
-  # Ingress mode: app draait op localhost:8123
+  # Ingress-mode: alleen localhost, HA-supervisor regelt TLS/HTTPS
   export FLASK_RUN_PORT=8123
   flask --app /app/dashboard run --host 127.0.0.1
 fi
