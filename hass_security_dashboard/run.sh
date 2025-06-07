709ad71c7eb59dd80ea7c77108b0d3f0610054f4
Dennis <102389032+dkwolf1@users.noreply.github.com>
2025-06-07 20:58:18 +0200

    Read options from env and pass CF token

--- hass_security_dashboard/run.sh
+++ hass_security_dashboard/run.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/with-contenv bashio
 export FLASK_APP=back/app.py
-flask run --host=0.0.0.0 --port=$(bashio::config 'port')
+
+# Read options from Hass.io configuration or environment variables
+export PORT="${PORT:-$(bashio::config 'port')}"
+export USE_INGRESS="${USE_INGRESS:-$(bashio::config 'use_ingress')}"
+export CLOUDFLARE_API_TOKEN="${CLOUDFLARE_API_TOKEN:-$(bashio::config 'cloudflare_api_token')}"
+
+flask run --host=0.0.0.0 --port="${PORT}"
