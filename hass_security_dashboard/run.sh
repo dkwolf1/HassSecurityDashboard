a545ffe9b5b1d024d58894e14f4c5eff75d78e36
Dennis <102389032+dkwolf1@users.noreply.github.com>
2025-06-08 10:59:07 +0200

    Add cloudflare_domain option

--- hass_security_dashboard/run.sh
+++ hass_security_dashboard/run.sh
@@ -5,6 +5,7 @@ export FLASK_APP=back/app.py
 export PORT="${PORT:-$(bashio::config 'port')}"
 export USE_INGRESS="${USE_INGRESS:-$(bashio::config 'use_ingress')}"
 export CLOUDFLARE_API_TOKEN="${CLOUDFLARE_API_TOKEN:-$(bashio::config 'cloudflare_api_token')}"
+export CLOUDFLARE_DOMAIN="${CLOUDFLARE_DOMAIN:-$(bashio::config 'cloudflare_domain')}"
 export DUCKDNS_DOMAIN="${DUCKDNS_DOMAIN:-$(bashio::config 'duckdns_domain')}"
 export CONFIG_PATH="${CONFIG_PATH:-$(bashio::config 'config_path')}"
 
